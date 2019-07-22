180f946fb18202736a2a0c744afc5786bcdd730a
mkearney <kearneymw@missouri.edu>
2019-07-22 14:12:59 -0500

    Initial commit

---
+++ R/pkgs-use-most.R
@@ -0,0 +1,81 @@
+
+#' Package use
+#'
+#' Take inventory of your own package use
+#'
+#' @param ... Directory location in which files ending in '.R' and '.Rmd' will
+#'   be searched for packages (via fully qualified namespace or calls to library
+#'   or require).
+#' @return A pkguse object, which is really just a tibble with specialized
+#'   methods for plot and summary.
+#' @export
+pkg_use <- function(...) {
+  x <- read_r_files(...)
+  x <- parse_r_pkgs(x)
+  x <- factor(x, levels = unique(x))
+  x <- table(pkg = x)
+  tibble::as_tibble(x) %>%
+    arrange_and_factor(pkg, -n) %>%
+    `class<-`(c("pkguse", "tbl_df", "tbl", "data.frame"))
+}
+
+
+read_r_files <- function(...) {
+  dirs <- list(...)
+  r <- unlist(lapply(dirs, list.files, pattern = "\\.(R|Rmd|Rmarkdown|rmd|r)$",
+    recursive = TRUE,
+    full.names = TRUE))
+  x <- unlist(lapply(r, tfse::readlines))
+
+}
+
+parse_r_pkgs <- function(x) {
+  ## via pkg::
+  c(tfse::regmatches_(x, "[[:alpha:]][[:alnum:]\\.]+(?=::)", drop = TRUE),
+    ## via library(pkg)
+    tfse::regmatches_(x, "(?<=library\\()[A-Za-z][[:alnum:]\\.]+(?=\\))", drop = TRUE),
+    ## via require(pkg)
+    tfse::regmatches_(x, "(?<=require\\()[A-Za-z][[:alnum:]\\.]+(?=\\))", drop = TRUE),
+    ## via requireNamespace('pkg')
+    tfse::regmatches_(x, "(?<=requireNamespace\\(')[A-Za-z][[:alnum:]\\.]+(?=\\')", drop = TRUE),
+    ## via requireNamespace("pkg")
+    tfse::regmatches_(x, "(?<=requireNamespace\\(\")[A-Za-z][[:alnum:]\\.]+(?=\\\")", drop = TRUE))
+}
+
+arrange_and_factor <- function(.x, cat, val) {
+  cat <- rlang::enquo(cat)
+  cat <- names(dplyr::select(.x, !!cat))
+  val <- rlang::enquo(val)
+  .x <- dplyr::arrange(.x, !!val)
+  .x[[cat]] <- factor(.x[[cat]], levels = rev(unique(.x[[cat]])))
+  .x
+}
+
+#' @export
+plot.pkguse <- function(x, n = 50, base_family = "Arial Narrow", base_size = 16) {
+  x %>%
+    utils::head(n) %>%
+    dplyr::mutate(eo = rep(c(TRUE, FALSE), nrow(.) / 1.5)[seq_len(nrow(.))]) %>%
+    ggplot2::ggplot(ggplot2::aes(x = pkg, y = n, fill = eo)) +
+    ggplot2::geom_col() +
+    ggplot2::coord_flip() +
+    ggplot2::scale_fill_manual(values = c("#707a8f", "greenyellow")) +
+    ggplot2::theme_gray(base_family = base_family, base_size = base_size) +
+    ggplot2::theme(legend.position = "none",
+      plot.title = ggplot2::element_text(face = "bold")) +
+    ggplot2::labs(x = NULL, y = NULL,
+      title = "#rstats packages I use the most")
+}
+
+#' @export
+summary.pkguse <- function(x) {
+  k <- as.character(x$pkg)
+  n <- x$n
+  cat("Total packages        :", nrow(x), fill = TRUE)
+  cat("Total uses            :", sum(n), fill = TRUE)
+  most <- paste0(k[1], " (", n[1], " times)")
+  mid <- paste0(k[nrow(x) %/% 2], " (", n[nrow(x) %/% 2], " times)")
+  cat("Most used package     :", most, fill = TRUE)
+  cat("Median used package   :", mid, fill = TRUE)
+}
+
---
+++ README.md
@@ -0,0 +1,86 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# pkguse
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+The goal of pkguse is to help users take inventory of their most used
+packages
+
+## Installation
+
+You can install the dev version of pkguse from
+[Github](https://github.com/mkearney/pkguse) with:
+
+``` r
+remotes::install_github("mkearney/pkguse")
+```
+
+## Example
+
+This is a basic use example:
+
+``` r
+## load {pkguse}
+library(pkguse)
+
+## supply dir locations
+p <- pkg_use(c("~/Documents", "~/Dropbox", "~/R"))
+#> Warning in readLines(encoding = "UTF-8", skipNul = TRUE, warn = FALSE, con
+#> = structure(4L, class = c("file", : invalid input found on input connection
+#> '/Users/kearneymw/R/boost/libs/thread/test/Carbon.r'
+
+## view output (top 25)
+print(p, n = 25)
+#> # A tibble: 350 x 2
+#>    pkg           n
+#>    <fct>     <int>
+#>  1 dplyr      1501
+#>  2 ggplot2     817
+#>  3 tfse        810
+#>  4 rtweet      699
+#>  5 rvest       656
+#>  6 readr       652
+#>  7 knitr       518
+#>  8 httr        486
+#>  9 tibble      453
+#> 10 dapr        305
+#> 11 rlang       284
+#> 12 purrr       255
+#> 13 tidyverse   218
+#> 14 xml2        201
+#> 15 jsonlite    184
+#> 16 base        160
+#> 17 devtools    151
+#> 18 tidyr       143
+#> 19 tbltools    140
+#> 20 rmarkdown   112
+#> 21 xgboost     103
+#> 22 MASS        102
+#> 23 igraph      101
+#> 24 mwk          98
+#> 25 usethis      96
+#> # … with 325 more rows
+```
+
+The output has methods for `plot`
+
+``` r
+## plot pkguse (top 25)
+plot(p, n = 25)
+```
+
+<img src="man/figures/README-plot-1.png" width="100%" />
+
+and `summary`
+
+``` r
+summary(p)
+#> Total packages        : 350
+#> Total uses            : 12922
+#> Most used package     : dplyr (1501 times)
+#> Median used package   : vctrs (4 times)
+```
